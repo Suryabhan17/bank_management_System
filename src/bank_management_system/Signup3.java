09f0b8ca3f7f32370555a8db3c8b4c82b21c1e51
Suryabhan17 <suryabhanuday99@gmail.com>
2025-09-16 16:28:58 +0530

    src/bank_management_system/Signup3.java

--- src/bank_management_system/Signup3.java
+++ src/bank_management_system/Signup3.java
@@ -48,6 +48,18 @@ Signup3( ){
   r3.setBounds(100,220,200,20);
   add(r3);
 
+  r4=new JRadioButton("Recurring Deposit Account");
+  r4.setFont(new Font("Raleway",Font.BOLD,16));
+  r4.setBounds(350,220,300,20);
+  r4.setBackground(Color.WHITE);
+  add(r4);
+
+  ButtonGroup groupacc=new ButtonGroup();
+  groupacc.add(r1);
+  groupacc.add(r2);
+  groupacc.add(r3);
+  groupacc.add(r4);
+
 
 
 
@@ -84,6 +96,37 @@ Signup3( ){
   services.setFont(new Font("Raleway",Font.BOLD,20));
   services.setBounds(100,450,200,30);
   add(services);
+
+  c1=new JCheckBox("ATM CARD");
+  c1.setBackground(Color.WHITE);
+  c1.setFont(new Font("Raleway",Font.BOLD,16));
+  c1.setBounds(100,500,200,30);
+  add(c1);
+
+
+  c2=new JCheckBox("Internet Banking");
+  c2.setBackground(Color.white);
+  c2.setFont(new Font("Raleway",Font.BOLD,16));
+  c2.setBounds(350,500,200,30);
+  add(c2);
+
+  c3=new JCheckBox("Mobile Banking");
+  c3.setBackground(Color.white);
+  c3.setFont(new Font("Raleway",Font.BOLD,16));
+  c3.setBounds(100,550,200,30);
+  add(c3);
+
+  c4=new JCheckBox("EMAIL Alerts");
+  c4.setBackground(Color.white);
+  c4.setFont(new Font("Raleway",Font.BOLD,16));
+  c4.setBounds(350,550,200,30);
+  add(c4);
+
+  c5 =new JCheckBox("Cheque Book");
+  c5.setBackground(Color.WHITE);
+  c5.setFont(new Font("Raleway",Font.BOLD,16));
+  c5.setBounds(100,600,200,30);
+  add(c5);
 
 
 
