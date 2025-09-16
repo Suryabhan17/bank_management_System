4f504790183de6e19bff6709033e0446d4cfc22b
Suryabhan17 <suryabhanuday99@gmail.com>
2025-09-17 00:58:43 +0530

    compleactionperformed

--- src/bank_management_system/Transactions.java
+++ src/bank_management_system/Transactions.java
@@ -8,7 +8,7 @@ public class Transactions extends JFrame implements ActionListener {
   JButton deposit, withdrow,fastcash,ministatement,pinchange,balanceenquiry,exit;
   String pinnumber;
 
-  Transactions(){
+  Transactions(String pinnumber){
 
     this.pinnumber=pinnumber;
     setLayout(null);
@@ -29,20 +29,44 @@ public class Transactions extends JFrame implements ActionListener {
     
     JLabel text=new JLabel("Please Select Your Transaction");
     text.setFont(new Font("System",Font.BOLD,22));
-    text.setBounds(180,215,400,30);
+    text.setBounds(180,220,400,30);
     text.setForeground(Color.white);
     image.add(text);
 
     deposit =new JButton("Deposit");
-    deposit.setBounds(160,317,140,30);
+    deposit.setBounds(160,317,140,25);
     deposit.addActionListener(this);
     image.add(deposit);
 
     withdrow =new JButton("Cash Withdrow");
-    withdrow.setBounds(360,317,140,30);
+    withdrow.setBounds(370,317,140,25);
     withdrow.addActionListener(this);
     image.add(withdrow);
 
+    fastcash =new JButton("Fast Cash");
+    fastcash.setBounds(160,350,140,25);
+    fastcash.addActionListener(this);
+    image.add(fastcash);
+
+    ministatement = new JButton("Mini Statement");
+    ministatement.setBounds(370,347,140,25);
+    ministatement.addActionListener(this);
+    image.add(ministatement);
+
+    pinchange =new JButton("PIN Change");
+    pinchange.setBounds(160,380,140,25);
+    pinchange.addActionListener(this);
+    image.add(pinchange);
+
+    balanceenquiry =new JButton("Balance Enquiry");
+    balanceenquiry.setBounds(370,375,140,25);
+    balanceenquiry.addActionListener(this);
+    image.add(balanceenquiry);
+
+    exit =new JButton("Exit");
+    exit.setBounds(370,405,140,25);
+    exit.addActionListener(this);
+    image.add(exit);
 
     setSize(900,700);
     setLocation(350,10);
@@ -51,12 +75,33 @@ public class Transactions extends JFrame implements ActionListener {
    
   }
   public void actionPerformed(ActionEvent ae){
+    if(ae.getSource()==exit){
+      System.exit(0);
+    }else if(ae.getSource() ==deposit){
+      setVisible(false);
+      new Deposit(pinnumber).setVisible(true);
+    }else if(ae.getSource()==withdrow){
+      setVisible(false);
+      new Withdraw(pinnumber).setVisible(true);
+
+    }else if(ae.getSource()==fastcash){
+        setVisible(false);
+        new FastCash(pinnumber).setVisible(true);
+    } else if (ae.getSource() == pinchange) {
+            setVisible(false);
+            new PinChange(pinnumber).setVisible(true);
+        } else if (ae.getSource() == balanceenquiry) {
+            setVisible(false);
+            new BalanceEnquiry(pinnumber).setVisible(true);
+        } else if (ae.getSource() == ministatement) {
+            new MiniStatement(pinnumber).setVisible(true);
+        }
 
   }
 
 public static void main(String[] args) {
 
-  new Transactions();
+  new Transactions("").setVisible(true);
 }
   
 }
